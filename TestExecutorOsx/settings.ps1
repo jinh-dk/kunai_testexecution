b3c1a031ffa239c17211a8d96081326a2cdbd214
jinxuunity <jinxu@unity3d.com>
2017-08-02 08:53:32 +0200

    add osx execution file

---
+++ TestExecutorOsx/settings.ps1
@@ -0,0 +1,26 @@
+﻿$testreportfolder = "C:\users\jinxu\Documents\GitHub\kunai\test\Log\"
+$testreportname = "integrationTest.xml"
+
+$testfwkfolder = "C:\Users\jinxu\Documents\GitHub\kunai\dev\itest\test\Publishing.IntegrationTests.Framework\"
+$testfwkproj = "Publishing.IntegrationTests.Framework.csproj"
+
+$testcasefolder_api = "C:\Users\jinxu\Documents\GitHub\kunai\dev\itest\test\Publishing.Api.IntegrationTest\"
+$testcaseproj_api = "Publishing.Api.IntegrationTest.csproj"
+
+$testresultfolder = "C:\Users\jinxu\Documents\GitHub\kunai\test\result\"
+
+$venvgooglefolder = ".\GoogleSpreadSheet\Scripts\"
+
+
+$masterfolder = "/Users/jinxu/codes/kunai/master/"
+$masterdockerfolder = "/Users/jinxu/codes/kunai/master/docker/"
+$masterfrontendfolder = "C:\Users\jinxu\Documents\GitHub\kunai\master\frontend\"
+$testfolder = "C:\Users\jinxu\Documents\GitHub\kunai\test\"
+
+$logfolder = "/Users/jinxu/codes/kunai/test/Log/"
+$backendlogfile = "BackendOsx.log"
+$backendlogfileid = "1iecekG5Rlnme9rfJ4PWP1KPUh1Kt8JOLIMcJHIeZmvk"
+$frontendlogfile = "Frontend.log"
+$frontendlogfileid = "1CmjkWFjPUQZJmEHqgnFhruFMyfR2Gxm8EDGmUKPaMbY"
+$updatemasterlogfile = "PullMaster.log"
+$errorcsvfile = "Error.csv"
