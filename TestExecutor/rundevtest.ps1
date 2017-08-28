77d0ff8ea1f7b3e57d215025ca17e1b4efb8f432
jinxuunity <jinxu@unity3d.com>
2017-08-28 14:58:23 +0200

    Correct the error and make dev-branch test automation works

--- TestExecutor/rundevtest.ps1
+++ TestExecutor/rundevtest.ps1
@@ -10,6 +10,8 @@ param(
 
 . .\settings.ps1
 
+$dockerfolder = "\docker"
+
 ## Stop current docker
 .\ManageKunaiBackendService.ps1 -action down
 
@@ -19,7 +21,9 @@ param(
 ## checkout master
 Push-Location $devmainfolder
 $foldername = "devtest$branchname"
-git clone $gitrepo $foldername
+if (-not (Test-Path $foldername)) {
+    git clone $gitrepo $foldername
+}
 Pop-Location
 
 
@@ -27,19 +31,24 @@ Pop-Location
 Push-Location $devmainfolder$foldername
 $cmd = "git checkout $branchname"
 iex $cmd
-
-## run docker compose
-$cmd = "docker-compose.exe -f docker\docker-compose-dbs.yml -f docker\docker-compose-full.yml up"
+$cmd = "git submodule update --init"
 iex $cmd
 Pop-Location
 
+## run docker compose, wait 10 minutes for initilization.
+Push-Location $devmainfolder$foldername$dockerfolder
+$cmd = "docker-compose.exe -f docker-compose-dbs.yml -f docker-compose-full.yml up"
+Invoke-Expression ('cmd /c start powershell -Command {{ {0}; }}' -f $cmd)
+Start-Sleep -Seconds 600
+Pop-Location
+
 ## Execute test
-.\ExecuteTest.ps1 -testcasepath $testfolder$testfolderapicase
+.\ExecuteTest.ps1 -testcasepath $testcasefolder_api
 
 ## Stop all the containers  
-Push-Location $devmainfolder$foldername
+Push-Location $devmainfolder$foldername$dockerfolder
 Write-Host $pwd
-$cmd = "docker-compose.exe -f docker\docker-compose-dbs.yml -f docker\docker-compose-full.yml down"
+$cmd = "docker-compose.exe -f docker-compose-dbs.yml -f docker-compose-full.yml down"
 iex $cmd
 Pop-Location
 
@@ -50,4 +59,5 @@ Pop-Location
 if ($RemoveBranchAfterTest){
     Push-Location $devmainfolder
     Remove-Item -Recurse -Force ".\$foldername"
+    Pop-Location
 }
