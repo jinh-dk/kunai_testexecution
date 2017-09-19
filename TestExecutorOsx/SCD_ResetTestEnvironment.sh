393dbee06fc42a3ac18cb940f6e9df0c4b2fd552
jinxuunity <jinxu@unity3d.com>
2017-09-19 12:56:25 +0200

    correct an error

--- TestExecutorOsx/SCD_ResetTestEnvironment.sh
+++ TestExecutorOsx/SCD_ResetTestEnvironment.sh
@@ -9,4 +9,4 @@ fi
 echo "Delete Database"
 powershell ../TestExecutor/DeleteDatabaseDocker.ps1
 
-echo echo -e "\033[0;31m Remember to rescan Kallithea after restart the test environment \033[0m"
+echo -e "\033[0;31m Remember to rescan Kallithea after restart the test environment \033[0m"
