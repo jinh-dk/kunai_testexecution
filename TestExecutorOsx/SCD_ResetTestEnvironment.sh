09876c49009cd3a2639025f4b740fda9033ab1ec
jinxuunity <jinxu@unity3d.com>
2017-09-19 12:53:51 +0200

    Update codes

--- TestExecutorOsx/SCD_ResetTestEnvironment.sh
+++ TestExecutorOsx/SCD_ResetTestEnvironment.sh
@@ -1,10 +1,12 @@
 #! /bin/env bash
 
+echo "Clean folder"
 if [ -n "$2" ] &&  [ "$2" == true ]; then     
     . SCD_CleanStripAgentDockerFoler.sh $1 $2
 else 
     . SCD_CleanStripAgentDockerFoler.sh
 fi 
+echo "Delete Database"
 powershell ../DeleteDatabaseDocker.ps1
 
 echo echo -e "\033[0;31m Remember to rescan Kallithea after restart the test environment \033[0m"
