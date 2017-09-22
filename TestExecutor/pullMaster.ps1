b35dffd23ee912a5a5d2494fe0bfd0207ae56a35
jinxuunity <jinxu@unity3d.com>
2017-09-22 10:37:50 +0200

    Add "git submodule update --recursive" to pullmaster.ps1

--- TestExecutor/pullMaster.ps1
+++ TestExecutor/pullMaster.ps1
@@ -2,6 +2,7 @@
 Push-Location $masterfolder
 Start-Transcript $logfolder$updatemasterlogfile
 git pull --rebase
+git submodule update --recursive
 dotnet restore
 #invoke-expression 'cmd /c start powershell -Command { .\build.ps1}'  #;Read-Host 
 .\build.ps1
